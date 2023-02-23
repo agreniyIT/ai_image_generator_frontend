9e6b5b107a69139bb1ca3ac46b36707626b19e18
agreniyIT <manojbittu161@gmail.com>
2023-02-23 20:19:12 +0530

    second

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
     
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://ai-image-api.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
